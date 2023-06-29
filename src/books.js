9a62511ede90bba56d4256ada2c0fcd2e3c444f5
Dongjae Heo <lindsay.heo@gmail.com>
2023-06-29 18:56:07 +1000

    done

---
+++ src/books.js
@@ -0,0 +1,24 @@
+import img1 from "./images/book1.jpg";
+import img2 from "./images/book2.jpg";
+import img3 from "./images/book3.jpg";
+
+export const books = [
+  {
+    author: "Johnny Joey Jones",
+    title: "Unbroken Bonds of Battle",
+    img: img1,
+    id: 1,
+  },
+  {
+    author: "Iron Flame",
+    title: "Rebecca Yarros",
+    img: img2,
+    id: 2,
+  },
+  {
+    author: "Atomic Habits",
+    title: "James Clear",
+    img: img3,
+    id: 3,
+  },
+];
